4cf4ae958b473ccb3b44a3f121e555132231d7a1
Chris Bratlien <chrisbratlien@gmail.com>
2023-10-15 07:54:51 -0500

    add button to JSONInspector toolbar UI to inject wsInspect into the window for use in the Dev Console JS REPL

--- js/JSONInspector.js
+++ js/JSONInspector.js
@@ -9,37 +9,58 @@ function JSONInspector(props) {
         title: "JSON Inspector",
         className: "json-inspector"
     });
-    /*
+
+
+
+    function injectWInspect() {
+        window.wInspect = wInspect;
+    }
+
     wInspect.appendToToolbar(
         DOM.button()
         .addClass('btn btn-sm')
         .append([
             DOM.i()
-            .addClass('fa fa-copy'),
+            .addClass('fa fa-glass'),
         ])
-        .on('click', inspector.copyTextToClipboard)
+        .on('click', injectWInspect)
     )
-    */
+    
+
+
     let phInspect = DOM.div()
     let phHistory = DOM.div()
         .addClass('history')    
+
+
+        //update the inner inspector object as well as the history
+        //and history UI
+    wInspect.update = function(anObject) {
+        history.push(anObject);
+
+        phHistory.append(
+            DOM.span("⏰")
+                .on('click',function(){
+                    inspector.update(anObject);
+                    inspector.renderOn(phInspect);
+                })
+        )
+        inspector.update(anObject);
+        inspector.renderOn(phInspect)
+
+    }
+    
+    
+
+
+
     wInspect.append([
         DOM.textarea()
             .addClass('full-width')
             .on('change',function(e){
                 //console.log(e.target.value);
                 var data = JSON.parse(e.target.value);
-                history.push(data);
-
-                phHistory.append(
-                    DOM.span("⏰")
-                        .on('click',function(){
-                            inspector.update(data);
-                            inspector.renderOn(phInspect);
-                        })
-                )
-                inspector.update(data);
-                inspector.renderOn(phInspect)
+                wInspect.update(data);
             }),
             phHistory,
             phInspect
